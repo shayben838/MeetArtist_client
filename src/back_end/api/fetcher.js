d25376cac84db5420a78e16bcfc9f0013d5bb3f2
shay <shayben838@gmail.com>
2020-05-10 22:59:42 +0300

    sd

--- src/back_end/api/fetcher.js
+++ src/back_end/api/fetcher.js
@@ -1,13 +1,27 @@
 import axios from "axios";
 
+
+// HEROKU
 const fetcher = axios.create({
     baseURL: "https://meetartistserver.herokuapp.com",
     withCredentials: true
 })
 
-
-
 export default fetcher;
+
+// LOCAL
+// const fetcher = axios.create({
+//     baseURL: "http://localhost:5000",
+//     withCredentials: true
+// })
+
+// export default fetcher;
+
+
+
+
+
+
 
 const fetcherCookies = axios.create({
     // const fetcherCookies = (token)=>axios.create({
