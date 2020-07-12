a54b29bce9de10d251bc3e3593f447db2654c9f7
shay <shayben838@gmail.com>
2020-07-12 16:40:43 +0300

    change cv

--- src/components/loading/loading_home.js
+++ src/components/loading/loading_home.js
@@ -1,15 +1,24 @@
 import React from "react";
-import "./loading.css"
-import style from "../../style_main/style"
+import "./loading.css";
+import style from "../../style_main/style";
 
 function LoadingHome() {
-    return (
-        <div >
-        <div className="wrap_loader" style={{background:style.mainColor}}>
-            <h1 className="lead loader_text">Welcome To Meet Artist</h1>
-            <div className="loader" style={{color:style.secondColor}}>Loading...</div>
+  return (
+    <div>
+      <div className='wrap_loader' style={{ background: style.mainColor }}>
+        <h1 className='lead loader_text' id='top'>
+          Welcome To Meet Artist
+        </h1>
+
+        <h1 className='lead loader_text' id='bottom'>
+          The place where musical collaborations are created.
+        </h1>
+
+        <div className='loader' style={{ color: style.secondColor }}>
+          Loading...
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 export default LoadingHome;
