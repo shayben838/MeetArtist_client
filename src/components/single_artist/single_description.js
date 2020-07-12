4d36a67d108e2dd0e6d03e82d24294f48760cea4
shay <shayben838@gmail.com>
2020-07-12 17:19:25 +0300

    change cv

--- src/components/single_artist/single_description.js
+++ src/components/single_artist/single_description.js
@@ -1,13 +1,23 @@
 import React from "react";
-import mainColor from "../../style_main/style"
-import "./wrap_single_artist.css"
+import mainColor from "../../style_main/style";
+import "./wrap_single_artist.css";
 
-function SingleDescription({ singleData ,name}) {
-    return (
-            <div className="row m-0 pl-2 ml-1">
-                 <span className=" headline mb-1 " style={{borderBottom:"1px solid #a1a1a1",textTransform:"capitalize",fontWeight:"bold"}}> {name}   </span>
-                 <span className="headline " style={{ color: `${mainColor.mainColor}` }}>: &nbsp; {singleData}</span>
-             </div>
-    )
+function SingleDescription({ singleData, name }) {
+  return (
+    <div className='row m-0 pl-2 ml-1'>
+      <span
+        className=' headline mb-1 '
+        style={{
+          textTransform: "capitalize",
+        }}
+      >
+        {" "}
+        {name}{" "}
+      </span>
+      <span className='headline ' style={{ color: `${mainColor.mainColor}` }}>
+        : &nbsp; {singleData}
+      </span>
+    </div>
+  );
 }
 export default SingleDescription;
